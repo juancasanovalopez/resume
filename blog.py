d0d4a2d292c77692802a6d9a0b648989b5547efe
Juan Casanova <juan.casanova@renault.com>
2018-08-29 09:02:58 +0200

    DB started

--- blog.py
+++ blog.py
@@ -1,1 +1,11 @@
-from app import app
+# https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-iv-database
+# http://ondras.zarovi.cz/sql/demo/
+# https://medium.com/@charlie.b.ohara/building-a-flask-rest-api-with-docker-94ca4219f460
+# https://docs.docker.com/network/links/
+
+from app import app, db
+from app.models import User, Post
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
