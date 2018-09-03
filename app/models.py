512cd0790a8795061ffd7b94151daefd69e4230a
Juan Casanova <juan.casanova@renault.com>
2018-09-03 13:57:04 +0200

    + Job model

--- app/models.py
+++ app/models.py
@@ -3,29 +3,26 @@ from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
-class User(db.Model):
+
+#######################################
+#   id
+#     category
+#     title
+#     subtitle
+#     location
+#     period
+#     description
+#
+#     comment here!
+#######################################
+class Job(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
-    posts = db.relationship('Post', backref='author', lazy='dynamic')
+    category = db.Column(db.String(140))
+    title = db.Column(db.String(140))
+    subtitle = db.Column(db.String(140))
+    location = db.Column(db.String(140))
+    period = db.Column(db.String(140))
+    description = db.Column(db.String(140))
 
     def __repr__(self):
-        return '<User {}>'.format(self.username)
-
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
-
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
-
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    body = db.Column(db.String(140))
-    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
-    def __repr__(self):
-        return '<Post {}>'.format(self.body)
-
-class User(UserMixin, db.Model):
+        return '<Job {}>'.format(self.title)
