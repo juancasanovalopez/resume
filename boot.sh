4672f84b90b4ed5f1ad6aa4f49a159810e338ebf
Juan <juan@casanovalopez.es>
2018-09-16 19:54:02 +0200

    fixin' .gitignored files & folders

--- boot.sh
+++ boot.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-source matrix/bin/activate
+source venv/bin/activate
 flask db upgrade
 flask translate compile
 exec gunicorn -b 8000:5000 --access-logfile - --error-logfile - resume:app
